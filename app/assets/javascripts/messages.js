85c95317e94497728d2e30e13de83de88502b62e
Vu Long <vulong264@gmail.com>
2016-11-01 20:36:42 +0700

    milestone 4 completed

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,2 +1,16 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+window.updateMessagesViaJSON = function() {
+  console.log("inside updateMessagesViaJSON");
+	json_path = window.location.pathname + ".json";
+  $.getJSON(json_path, function(messages) {
+    e = $('.messages');
+		e.html('');
+		for (i in messages)
+		{
+			message = messages[i];
+			e.append("<li class='list-group-item'><span class='tag tag-default tag-pill float-xs-right'>12</span>"
+			+ message.content +"</li>");
+		}
+  })
+}
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,11 @@ class MessagesController < ApplicationController
 	def index
 		load_room
 		@messages = @room.messages
+
+		respond_to do |format|
+			format.html
+			format.json {render json: @messages}
+		end
 	end
 
 	def create
